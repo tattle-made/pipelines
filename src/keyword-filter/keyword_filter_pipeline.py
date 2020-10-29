1eb6524b438be278d4457b8ca37a9f581c0fb2d2
Kruttika Nadig <kruttika17@gmail.com>
2020-10-29 11:40:28 +0530

    added eror handling, limited n docs for debugging

--- src/keyword-filter/keyword_filter_pipeline.py
+++ src/keyword-filter/keyword_filter_pipeline.py
@@ -34,23 +34,28 @@ class SourceData(luigi.Task):
         return luigi.LocalTarget('urls.txt')
         
     def run(self):
-        logger.info("Beginning SourceData() task ...")
-        logger.info("Getting image urls from MongoDB ...")
-        client = initialize_mongo()
-        target = MongoCollectionTarget(client, self.db, self.collection)
-        coll = target.get_collection()
-        end = datetime.utcnow() 
-        start = end - timedelta(days=1)
-        with self.output().open("w") as out_file:
-            dump = {}
-            for i in coll.find({"scraped_date": {'$gte':start,'$lt':end}}): #limit for testing
-                if i["media_type"] == "image":
-                    url = i["s3_url"]
-                    doc_id = str(i["_id"])
-                    dump[doc_id] = url
-            out_file.write(json.dumps(dump))
-            logger.info("SourceData() task complete")
-            logger.info("Image urls written to out_file")
+        try:
+            logger.info("Beginning SourceData() task ...")
+            logger.info("Getting image urls from MongoDB ...")
+            client = initialize_mongo()
+            target = MongoCollectionTarget(client, self.db, self.collection)
+            coll = target.get_collection()
+            end = datetime.utcnow() 
+            start = end - timedelta(days=1)
+            with self.output().open("w") as out_file:
+                dump = {}
+                for i in coll.find({"scraped_date": {'$gte':start,'$lt':end}}).limit(10): #limit for testing
+                    if i["media_type"] == "image":
+                        url = i["s3_url"]
+                        doc_id = str(i["_id"])
+                        dump[doc_id] = url
+                out_file.write(json.dumps(dump))
+                logger.info("SourceData() task complete")
+                logger.info("Image urls written to out_file")
+        except Exception:
+            logger.info("Problem sourcing data")
+            logger.info(logging.traceback.format_exc())
+            pass
 
 
 class ExtractText(luigi.Task):
@@ -65,21 +70,26 @@ class ExtractText(luigi.Task):
         return luigi.LocalTarget("extracted_text.txt")
 
     def run(self):
-        logger.info("Beginning ExtractText() task ...")
-        logger.info("Extracting text from images ...")
-        get_credentials()
-        client = initialize_googleapi()
-        with self.output().open("w") as out_file:
-            dump = {}
-            with self.input().open("r") as in_file:
-                for mongo_data in in_file:
-                    mongo_data = json.loads(mongo_data)
-                    for doc_id, url in mongo_data.items():
-                        text = extract_text(client, url)
-                        dump[doc_id] = text
-            out_file.write(json.dumps(dump))
-            logger.info("ExtractText() task complete")
-            logger.info("Extracted text written to out_file")
+        try:
+            logger.info("Beginning ExtractText() task ...")
+            logger.info("Extracting text from images ...")
+            get_credentials()
+            client = initialize_googleapi()
+            with self.output().open("w") as out_file:
+                dump = {}
+                with self.input().open("r") as in_file:
+                    for mongo_data in in_file:
+                        mongo_data = json.loads(mongo_data)
+                        for doc_id, url in mongo_data.items():
+                            text = extract_text(client, url)
+                            dump[doc_id] = text
+                out_file.write(json.dumps(dump))
+                logger.info("ExtractText() task complete")
+                logger.info("Extracted text written to out_file")
+        except Exception:
+            logger.info("Problem extracting text")
+            logger.info(logging.traceback.format_exc())
+            pass
 
     def requires(self):
         return SourceData(self.db, self.collection)
@@ -98,19 +108,24 @@ class TranslateText(luigi.Task):
     def run(self):
         logger.info("Beginning TranslateText() task ...")
         logger.info("Transating text to English ...")
-        translator = initialize_translator()
-        with self.output().open("w") as out_file:
-            dump = {}
-            with self.input().open("r") as in_file:
-                for extracted_text in in_file:
-                    extracted_text = json.loads(extracted_text)
-                    for doc_id,text in extracted_text.items():
-                        translation = translate_text(text, translator)
-                        dump[doc_id] = translation 
-                        time.sleep(uniform(3,5))
-            out_file.write(json.dumps(dump))
-            logger.info("TranslateText() task complete")
-            logger.info("Translated text written to out_file")
+        try:
+            translator = initialize_translator()
+            with self.output().open("w") as out_file:
+                dump = {}
+                with self.input().open("r") as in_file:
+                    for extracted_text in in_file:
+                        extracted_text = json.loads(extracted_text)
+                        for doc_id,text in extracted_text.items():
+                            translation = translate_text(text, translator)
+                            dump[doc_id] = translation 
+                            time.sleep(uniform(3,5))
+                out_file.write(json.dumps(dump))
+                logger.info("TranslateText() task complete")
+                logger.info("Translated text written to out_file")
+        except Exception:
+            logger.info("Problem translating text")
+            logger.info(logging.traceback.format_exc())
+            pass
               
     def requires(self):
         return ExtractText(self.db, self.collection)
@@ -129,19 +144,24 @@ class FilterText(luigi.Task):
         return luigi.LocalTarget("filtered_text.txt")
 
     def run(self):
-        logger.info("Beginning FilterText() task ...")
-        logger.info("Applying keyword filter to text ...")
-        with self.output().open("w") as out_file:
-            dump = {}
-            with self.input().open("r") as in_file:
-                for translated_text in in_file:
-                    translated_text = json.loads(translated_text)
-                    for doc_id,translation in translated_text.items():
-                        label = filter_text(translation)
-                        dump[doc_id] = label
-            out_file.write(json.dumps(dump))
-            logger.info("FilterText() task complete")
-            logger.info("Keyword filter labels written to out_file")
+        try:
+            logger.info("Beginning FilterText() task ...")
+            logger.info("Applying keyword filter to text ...")
+            with self.output().open("w") as out_file:
+                dump = {}
+                with self.input().open("r") as in_file:
+                    for translated_text in in_file:
+                        translated_text = json.loads(translated_text)
+                        for doc_id,translation in translated_text.items():
+                            label = filter_text(translation)
+                            dump[doc_id] = label
+                out_file.write(json.dumps(dump))
+                logger.info("FilterText() task complete")
+                logger.info("Keyword filter labels written to out_file")
+        except Exception:
+            logger.info("Problem filtering text")
+            logger.info(logging.traceback.format_exc())
+            pass
                         
     def requires(self):
         return TranslateText(self.db, self.collection)
@@ -160,22 +180,27 @@ class StoreExtraction(luigi.Task):
         #return MockTarget("StoreExtraction", mirror_on_stderr=True)
 
     def run(self):
-        logger.info("Beginning StoreExtraction() task ...")
-        logger.info("Storing extracted text in MongoDB ...")
-        client = initialize_mongo()
-        dump = {}
-        with self.input().open("r") as in_file:
-            for extracted_text in in_file:
-                extracted_text = json.loads(extracted_text)
-                for doc_id,text in extracted_text.items():
-                    # target = MongoCellTarget(client, self.db, self.collection, doc_id, self.field)
-                    # target.write(label)
-                    dump[doc_id] = text
-        doc_ids = list(dump.keys())
-        target = MongoRangeTarget(client, self.db, self.collection, doc_ids, self.field)
-        target.write(dump)
-        logger.info("StoreExtraction() task complete")
-        logger.info("{} image text extractions stored in MongoDB".format(len(doc_ids)))
+        try:
+            logger.info("Beginning StoreExtraction() task ...")
+            logger.info("Storing extracted text in MongoDB ...")
+            client = initialize_mongo()
+            dump = {}
+            with self.input().open("r") as in_file:
+                for extracted_text in in_file:
+                    extracted_text = json.loads(extracted_text)
+                    for doc_id,text in extracted_text.items():
+                        # target = MongoCellTarget(client, self.db, self.collection, doc_id, self.field)
+                        # target.write(label)
+                        dump[doc_id] = text
+            doc_ids = list(dump.keys())
+            target = MongoRangeTarget(client, self.db, self.collection, doc_ids, self.field)
+            target.write(dump)
+            logger.info("StoreExtraction() task complete")
+            logger.info("{} image text extractions stored in MongoDB".format(len(doc_ids)))
+        except Exception:
+            logger.info("Problem storing extracted text")
+            logger.info(logging.traceback.format_exc())
+            pass
 
         # Write a dummy output file so that StoreLabel's dependency is fulfilled
         with self.output().open("w") as out_file:
@@ -197,22 +222,27 @@ class StoreTranslation(luigi.Task):
         #return MockTarget("StoreTranslation", mirror_on_stderr=True)
 
     def run(self):
-        logger.info("Beginning StoreTranslation() task ...")
-        logger.info("Storing translated text in MongoDB ...")
-        client = initialize_mongo()
-        dump = {}
-        with self.input().open("r") as in_file:
-            for translated_text in in_file:
-                translated_text = json.loads(translated_text)
-                for doc_id,text in translated_text.items():
-                    # target = MongoCellTarget(client, self.db, self.collection, doc_id, self.field)
-                    # target.write(label)
-                    dump[doc_id] = text
-        doc_ids = list(dump.keys())
-        target = MongoRangeTarget(client, self.db, self.collection, doc_ids, self.field)
-        target.write(dump)
-        logger.info("StoreTranslation() task complete")
-        logger.info("{} image text translations stored in MongoDB".format(len(doc_ids)))
+        try:
+            logger.info("Beginning StoreTranslation() task ...")
+            logger.info("Storing translated text in MongoDB ...")
+            client = initialize_mongo()
+            dump = {}
+            with self.input().open("r") as in_file:
+                for translated_text in in_file:
+                    translated_text = json.loads(translated_text)
+                    for doc_id,text in translated_text.items():
+                        # target = MongoCellTarget(client, self.db, self.collection, doc_id, self.field)
+                        # target.write(label)
+                        dump[doc_id] = text
+            doc_ids = list(dump.keys())
+            target = MongoRangeTarget(client, self.db, self.collection, doc_ids, self.field)
+            target.write(dump)
+            logger.info("StoreTranslation() task complete")
+            logger.info("{} image text translations stored in MongoDB".format(len(doc_ids)))
+        except Exception:
+            logger.info("Problem storing translated text")
+            logger.info(logging.traceback.format_exc())
+            pass
 
         # Write a dummy output file so that StoreLabel's dependency is fulfilled
         with self.output().open("w") as out_file:
@@ -231,34 +261,39 @@ class StoreLabel(luigi.Task):
     field = "keyword_label"
 
     def run(self):
-        logger.info("Beginning StoreLabel() task ...")
-        logger.info("Storing keyword filter labels in MongoDB ...")
-        client = initialize_mongo()
-        dump = {}
-        with self.input().open("r") as in_file:
-            for filtered_text in in_file:
-                filtered_text = json.loads(filtered_text)
-                for doc_id,label in filtered_text.items():
-                    # target = MongoCellTarget(client, self.db, self.collection, doc_id, self.field)
-                    # target.write(label)
-                    # target.exists()
-                    dump[ObjectId(doc_id)] = label
-        doc_ids = list(dump.keys())
-        target = MongoRangeTarget(client, self.db, self.collection, doc_ids, self.field)
-        target.write(dump)
-        target.exists()
-        logger.info("StoreLabel() task complete")
-        logger.info("{} new labels stored in MongoDB".format(len(doc_ids)))
-        logger.info("{} posts contain keywords".format(sum(value == 1 for value in dump.values())))
-        logger.info("Clearing all out_files ...")
-        os.remove("urls.txt")
-        os.remove("extracted_text.txt")
-        os.remove("translated_text.txt")
-        os.remove("filtered_text.txt")
-        os.remove("dummy_extraction.txt")
-        os.remove("dummy_translation.txt")
-        os.remove("credentials.json")
-        logger.info("Out_files cleared")
+        try:
+            logger.info("Beginning StoreLabel() task ...")
+            logger.info("Storing keyword filter labels in MongoDB ...")
+            client = initialize_mongo()
+            dump = {}
+            with self.input().open("r") as in_file:
+                for filtered_text in in_file:
+                    filtered_text = json.loads(filtered_text)
+                    for doc_id,label in filtered_text.items():
+                        # target = MongoCellTarget(client, self.db, self.collection, doc_id, self.field)
+                        # target.write(label)
+                        # target.exists()
+                        dump[ObjectId(doc_id)] = label
+            doc_ids = list(dump.keys())
+            target = MongoRangeTarget(client, self.db, self.collection, doc_ids, self.field)
+            target.write(dump)
+            target.exists()
+            logger.info("StoreLabel() task complete")
+            logger.info("{} new labels stored in MongoDB".format(len(doc_ids)))
+            logger.info("{} posts contain keywords".format(sum(value == 1 for value in dump.values())))
+            logger.info("Clearing all out_files ...")
+            os.remove("urls.txt")
+            os.remove("extracted_text.txt")
+            os.remove("translated_text.txt")
+            os.remove("filtered_text.txt")
+            os.remove("dummy_extraction.txt")
+            os.remove("dummy_translation.txt")
+            os.remove("credentials.json")
+            logger.info("Out_files cleared")
+        except Exception:
+            logger.info("Problem storing label")
+            logger.info(logging.traceback.format_exc())
+            pass
                         
     def requires(self):
         return FilterText(self.db, self.collection)
